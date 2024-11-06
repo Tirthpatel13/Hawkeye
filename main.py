12fc0ff7b9377172f6eb00aae3ebfc14b9a1e4bc
Tirthpatel13 <tirthpatel131101@gmail.com>
2024-11-06 18:15:02 -0500

    face detecting by landmark points(circle)

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import cv2
-import mediapipe as m
+import mediapipe as mp
+import pyautogui
 c = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 while True:
     _, frame = c.read()
     frame = cv2.flip(frame, 1)
-    cv2.imshow('Hawkeye', frame)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    o= face_mesh.process(rgb_frame)
+    landmark = o.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
+    if landmark:
+        landmarks = landmark[0].landmark
+        for landmark in landmarks:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
+
+    cv2.imshow('Eye Controlled Mouse', frame)
     cv2.waitKey(1)
